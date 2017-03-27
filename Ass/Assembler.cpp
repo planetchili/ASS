800cd341e68da99fbafe2b03cc8d25de07f6f578
planetchili <planetchili@users.noreply.github.com>
2017-03-27 17:52:02 +0900

    add jz aliases

--- Ass/Assembler.cpp
+++ Ass/Assembler.cpp
@@ -5,10 +5,12 @@ void Assembler::RegisterOperations()
 {
 	RegisterInstruction<ImmediateJumpTemplate<0b011>>( "jmp" );
 	RegisterInstruction<ImmediateJumpTemplate<0b000>>( "je" );
+	RegisterAlias( "je","jz" );
 	RegisterInstruction<ImmediateJumpTemplate<0b001>>( "ja" );
 	RegisterInstruction<ImmediateJumpTemplate<0b010>>( "jb" );
 	RegisterAlias( "jb","jnc" );
 	RegisterInstruction<ImmediateJumpTemplate<0b100>>( "jne" );
+	RegisterAlias( "jne","jnz" );
 	RegisterInstruction<ImmediateJumpTemplate<0b101>>( "jna" );
 	RegisterInstruction<ImmediateJumpTemplate<0b110>>( "jnb" );
 	RegisterAlias( "jnb","jc" );
