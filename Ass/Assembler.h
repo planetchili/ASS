1e71fdc1b5d5f737ad8290326dc51e6a1650b9bd
planetchili <planetchili@users.noreply.github.com>
2017-03-27 13:40:16 +0900

    well, it builds...

---
+++ Ass/Assembler.h
@@ -0,0 +1,413 @@
+#pragma once
+
+#include "Trim.h"
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <variant>
+#include <optional>
+#include <sstream>
+#include <exception>
+#include <locale>
+#include <vector>
+#include <memory>
+#include <iomanip>
+#include <assert.h>
+#include "Parsing.h"
+
+class Instruction
+{
+public:
+	virtual ~Instruction() = 0;
+	virtual void Process( class Assembler& ass,std::string rest,int line ) const = 0;
+	virtual const std::string& GetName() const = 0;
+	bool operator<( const Instruction& other ) const
+	{
+		return GetName() < other.GetName();
+	}
+};
+
+class Directive
+{
+public:
+	virtual ~Directive() = 0;
+	virtual void Process( class Assembler& ass,std::string rest,int line ) const = 0;
+	virtual void Process( class Assembler& ass,std::string label,std::string rest,int line ) const
+	{
+		throw std::exception( (std::string( "Directive [" ) + GetName() + "] does not support labels [" + label + "]").c_str() );
+	}
+	virtual const std::string& GetName() const = 0;
+	bool operator<( const Directive& other ) const
+	{
+		return GetName() < other.GetName();
+	}
+};
+
+class Symbol
+{
+public:
+	enum class Type
+	{
+		Label,
+		Variable
+	};
+public:
+	Symbol( Symbol&& donor )
+		:
+		name( std::move( donor.name ) ),
+		type( donor.type ),
+		address( donor.address )
+	{}
+	int GetAddress() const { return address; }
+	const std::string& GetName() const { return name; }
+	Type GetType() const
+	{
+		return type;
+	}
+	int GetLine() const
+	{
+		return line;
+	}
+	static Symbol MakeVariable( const std::string& name,int line,int address )
+	{
+		return Symbol( name,address,line,Type::Variable );
+	}
+	static Symbol MakeLabel( const std::string& name,int line,int address )
+	{
+		return Symbol( name,address,line,Type::Label );
+	}
+	bool operator<( const Symbol& other ) const
+	{
+		return name < other.name;
+	}
+	bool operator==( const Symbol& other ) const
+	{
+		return type == other.type && name == other.name;
+	}
+private:
+	Symbol( const std::string& name,int line,int address,Type type )
+		:
+		address( address ),
+		name( name ),
+		type( type ),
+		line( line )
+	{}
+private:
+	Type type;
+	int address;
+	int line;
+	std::string name;
+};
+
+class Assembler
+{
+public:
+	Assembler( const std::string& name )
+	{
+		RegisterOperations();
+
+		std::ifstream file;
+		file.exceptions( std::ifstream::failbit | std::ifstream::badbit );
+		file.open( name );
+
+		int lineNum = 0;
+		while( true )
+		{
+			std::string line;
+			file >> line;
+			if( file.eof() )
+			{
+				break;
+			}			
+			sourceLines.emplace_back( line,lineNum++ );
+		}
+	}
+	void Emit( unsigned char byte )
+	{
+		ValidateAddress();
+		ram[address++] = byte;
+	}
+	void SetAddress( int address )
+	{
+		this->address = address;
+		ValidateAddress();
+	}
+	int GetAddress() const
+	{
+		ValidateAddress();
+		return address;
+	}
+	void AddLabel( const std::string& name,int address,int line )
+	{
+		symbols.emplace( name,Symbol::MakeLabel( name,line,address ) );
+	}
+	void AddVariable( const std::string& name,int address,int line )
+	{
+		symbols.emplace( name,Symbol::MakeVariable( name,line,address ) );
+	}
+	void Assemble( std::string& name )
+	{
+		bool error = false;
+		for( auto lp : sourceLines )
+		{
+			try
+			{
+				ProcessLine( lp.first,lp.second );
+			}
+			catch( const std::exception& e )
+			{
+				error = true;
+				mout << e.what() << std::endl;
+			}
+		}
+
+		if( error )
+		{
+			throw std::exception( "Assembly halted before dependency resolution due to errors!" );
+		}
+
+		mout << "Source code successfully processed." << std::endl;
+
+		/// resolve dependencies!!!
+		// ////////////////////////
+		ResolveDependencies();
+
+		mout << "Symbol dependencies successfuly resolved." << std::endl;
+
+		std::ofstream file;
+		file.exceptions( std::ifstream::failbit | std::ifstream::badbit );
+		file.open( name );
+		for( auto b : ram )
+		{
+			file << std::hex << std::setw( 2 ) << b.value_or( 0 ) << " ";
+		}
+
+		mout << "Binary image successfully generated." << std::endl;
+	}
+
+private:
+	//std::optional<const Symbol&> GetSymbol( const std::string& name ) const
+	//{
+	//	const auto i = symbols.find( name );
+	//	if( i != symbols.end() )
+	//	{
+	//		return i->second;
+	//	}
+	//	return {};
+	//}
+	//std::optional<const Symbol&> GetLabel( const std::string& name ) const
+	//{
+	//	const auto i = symbols.find( name );
+	//	if( i != symbols.end() && i->second.GetType() == Symbol::Type::Label )
+	//	{
+	//		return i->second;
+	//	}
+	//	return {};
+	//}
+	//std::optional<const Symbol&> GetVariable( const std::string& name ) const
+	//{
+	//	const auto i = symbols.find( name );
+	//	if( i != symbols.end() && i->second.GetType() == Symbol::Type::Variable )
+	//	{
+	//		return i->second;
+	//	}
+	//	return {};
+	//}
+	//void RemoveSymbol( const std::string& name )
+	//{
+	//	const int n = symbols.erase( name );
+	//	assert( n > 0 );
+	//}
+	void ProcessLine( std::string& line,int lineNum )
+	{
+		strip_comment( line );
+
+		{
+			auto t = extract_token_white( line );
+
+			if( !t.has_value() )
+			{
+				return;
+			}
+
+			if( is_label( t.value() ) )
+			{
+				// remove the colon
+				t.value().pop_back();
+				if( symbols.count( t.value() ) > 0 )
+				{
+					std::stringstream msg;
+					msg << "Cannot create label at line <" << lineNum << ">! Symbol [" << t.value() << "] already exists!";
+					throw std::exception( msg.str().c_str() );
+				}
+
+				if( extract_token_white( line ).has_value() )
+				{
+					std::stringstream msg;
+					msg << "Cannot create label at line <" << lineNum << ">! There is other garbage on that line!";
+					throw std::exception( msg.str().c_str() );
+				}
+				// add label
+				AddLabel( t.value(),GetAddress(),lineNum );
+			}
+			else if( is_name( t.value() ) )
+			{
+				if( symbols.count( t.value() ) > 0 )
+				{
+					std::stringstream msg;
+					msg << "Found a symbol at line <" << lineNum << ">! Symbol [" << t.value() << "] already exists and I hate you!";
+					throw std::exception( msg.str().c_str() );
+				}
+				else if( instructions.count( t.value() ) > 0 )
+				{
+					// process instruction
+					instructions[t.value()]->Process( *this,std::move( line ),lineNum );
+				}
+				else // not instruction; must be a directive!
+				{
+					auto d = extract_token_white( line );
+					if( !d.has_value() )
+					{
+						std::stringstream msg;
+						msg << "Found a symbol at line <" << lineNum << ">! Symbol [" << t.value() << "]. Expected directive afterwards. Found nothing. Is sad now.";
+						throw std::exception( msg.str().c_str() );
+					}
+					else if( !is_directive( d.value() ) )
+					{
+						std::stringstream msg;
+						msg << "Found a symbol at line <" << lineNum << ">! Symbol [" << t.value() << "]. Expected directive afterwards. Found ["
+							<< d.value() << "]. Wtf bro?";
+						throw std::exception( msg.str().c_str() );
+					}
+					else
+					{
+						// strip off directive dot
+						d.value().erase( d.value().begin(),d.value().begin() + 1 );
+						if( directives.count( d.value() ) == 0 )
+						{
+							std::stringstream msg;
+							msg << "Unknown directive at line <" << lineNum << ">! After new symbol [" << t.value() << "], found [."
+								<< d.value() << "]. Do you even code bro?";
+							throw std::exception( msg.str().c_str() );
+						}
+						// process labeled directive
+						directives[d.value()]->Process( *this,t.value(),std::move( line ),lineNum );
+					}
+				}
+			}
+			else if( !is_directive( t.value() ) )
+			{
+				// strip off directive dot
+				t.value().erase( t.value().begin(),t.value().begin() + 1 );
+				if( directives.count( t.value() ) == 0 )
+				{
+					std::stringstream msg;
+					msg << "Unknown directive at line <" << lineNum << ">! Found [."
+						<< t.value() << "]. tsk tsk.";
+					throw std::exception( msg.str().c_str() );
+				}
+				// process directive
+				directives[t.value()]->Process( *this,std::move( line ),lineNum );
+			}
+			else
+			{
+				std::stringstream msg;
+				msg << "What is this? I can't even. at line <" << lineNum << ">! It sez [" << t.value() << "].";
+				throw std::exception( msg.str().c_str() );
+			}
+		}
+	}
+	void ResolveDependencies()
+	{
+		// loop through all symbols and resolve their dependencies
+		for( auto it = symbols.cbegin(),next_it = symbols.cbegin(); 
+			it != symbols.cend(); it = next_it )
+		{
+			next_it = std::next( it );
+
+			// remove all matching references after writing in address in ram
+			bool unreferenced = true;
+			auto ir = symbolReferences.find( it->first );
+			if( ir != symbolReferences.end() )
+			{
+				while( ir->first == it->first )
+				{
+					if( ir->second.GetType() == it->second.GetType() )
+					{
+						// fill ram with referenced address
+						ram[ir->second.GetAddress()] = it->second.GetAddress();
+						// erase reference
+						unreferenced = false;
+						const auto here = ir++;
+						symbolReferences.erase( here );
+					}
+					else
+					{
+						mout << "Reference <" << ir->first << "> from line ["
+							<< ir->second.GetLine() << "] does not match symbol <"
+							<< it->first << "> defined at line [" << it->second.GetLine()
+							<< "]" << std::endl;
+					}
+				}
+			}
+			// erase symbol if it was referenced at least once
+			if( !unreferenced )
+			{
+				symbols.erase( it );
+			}
+		} // end of symbol resolution loop
+
+		// show warnings for unrefd symbols
+		for( auto& sp : symbols )
+		{
+			mout << "(warning) Unreferenced symbol <" << sp.first << "> declared "
+				<< "at line [" << sp.second.GetLine() << "]" << std::endl;
+		}
+
+		// show errors for unresolved references
+		for( auto& rp : symbolReferences )
+		{
+			mout << "Unresolved reference <" << rp.first << "> referenced "
+				<< "at line [" << rp.second.GetLine() << "] !!" << std::endl;
+		}
+
+		if( symbolReferences.size() > 0 )
+		{
+			throw std::exception( "Halting assembly due to unresolved references!!" );
+		}		
+	}
+	void ValidateAddress() const
+	{
+		if( address < 0 || address > 255 )
+		{
+			std::stringstream msg;
+			msg << "Assembling address out of bounds: " << std::hex << address;
+			throw std::exception( msg.str().c_str() );
+		}
+	}
+	void RegisterOperations()
+	{
+
+	}
+	template<class T>
+	void RegisterInstruction()
+	{
+		assert( instructions.count( T::name ) == 0 );
+		instructions.emplace( T::name,T{} );
+	}
+	template<class T>
+	void RegisterDirective()
+	{
+		assert( directives.count( T::name ) == 0 );
+		directives.emplace( T::name,T{} );
+	}
+private:
+	std::ostream& mout = std::cout;
+	std::map<std::string,Symbol> symbols;
+	std::multimap<std::string,Symbol> symbolReferences;
+	std::map<std::string,std::unique_ptr<Instruction>> instructions;
+	std::map<std::string,std::unique_ptr<Directive>> directives;
+	std::vector<std::pair<std::string,int>> sourceLines;
+	std::vector<std::optional<unsigned char>> ram;
+	int address = 0;
+};
---
+++ Ass/Parsing.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <string>
+#include <optional>
+#include <algorithm>
+#include "Trim.h"
+
+
+void strip_comment( std::string& line )
+{
+	const auto pos = line.find_first_of( ';' );
+	if( pos != std::string::npos )
+	{
+		line.erase( pos );
+	}
+}
+
+std::optional<std::string> extract_token_white( std::string& s )
+{
+	ltrim( s );
+	if( s.size() > 0 )
+	{
+		const auto i = std::find_if( s.begin(),s.end(),[]( char c ) { return c == ' ' || c == '\t'; } );
+		if( i != s.end() )
+		{
+			auto r = std::string( s.begin(),i );
+			s.erase( s.begin(),i );
+			return r;
+		}
+		else
+		{
+			return std::move( s );
+		}
+	}
+	return {};
+}
+
+bool is_label( const std::string& s )
+{
+	return s.size() > 1 &&
+		s.back() == ':' &&
+		!std::isdigit( s.front() ) &&
+		std::all_of( s.begin(),std::prev( s.end() ),[]( char c )
+	{
+		return std::isalnum( c ) || c == '_';
+	}
+	);
+}
+
+bool is_name( const std::string& s )
+{
+	return s.size() > 0 &&
+		!std::isdigit( s.front() ) &&
+		std::all_of( s.begin(),s.end(),[]( char c )
+	{
+		return std::isalnum( c ) || c == '_';
+	}
+	);
+}
+
+bool is_directive( const std::string& s )
+{
+	return s.size() > 1 &&
+		s.front() == '.' &&
+		std::all_of( s.begin() + 1,s.end(),
+			[]( char c ) { return std::isalnum( c ); }
+	);
+}
